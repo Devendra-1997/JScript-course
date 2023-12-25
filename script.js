c0f3187f3ade20977ed29e83ef4eb8d98c0de0fb
Devendra-1997 <devendradhakal92@gmail.com>
2023-12-26 05:13:45 +1100

    explored more about conditional statements

--- script.js
+++ script.js
@@ -76,12 +76,80 @@
 // console.log("cond1 && cond2", a > b && a === 6);
 
 // logical OR
-let a = 6;
-let b = 4;
+// let a = 6;
+// let b = 4;
 
-let cond1 = a > b;
-let cond2 = a === 6;
+// let cond1 = a > b;
+// let cond2 = a === 6;
 
-console.log(" a = ", a, "& b = ", b);
-// console.log("cond1 && cond2", cond1 || cond2);
-console.log("cond1 && cond2", a > b && a === 6);
+// console.log(" a = ", a, "& b = ", b);
+// // console.log("cond1 && cond2", cond1 || cond2);
+// console.log("cond1 && cond2", a > b && a === 6);
+
+// WE LEARNED OPERATORS IN ORDER TO CHECK THE CONDITIONS
+
+// conditional statements
+// to implement some condition in the code
+
+// if statement
+// let age = 25;
+// // if (age > 18) {
+// //   console.log("you can drive");
+// // }
+// if (age < 18) {
+//   console.log("you can drive");
+// }
+
+// let mode = "light";
+// let color;
+// if (mode === "dark") {
+//   color = "black";
+// }
+
+// if (mode === "light") {
+//   color = "white";
+// }
+// console.log(color);
+
+// if-else statements
+// let mode = "light";
+// let color;
+// if (mode === "dark") {
+//   color = "black";
+// } else {
+//   color = "white";
+// }
+// console.log(color);
+
+// odd or even
+// let num = 13;
+// if (num % 2 === 0) {
+//   console.log(num, "is even");
+// } else {
+//   console.log(num, "is odd");
+// }
+
+// syntax = rules for writing code
+
+// else-if statement
+
+// let age = 20;
+
+// if (age < 18) {
+//   console.log("junior");
+// }
+
+// let mode = "pink";
+// let color;
+
+// if (mode === "dark") {
+//   color = "black";
+// } else if (mode === "blue") {
+//   color = "blue";
+// } else if (mode === "pink") {
+//   color = "pink";
+// } else {
+//   color = "white";
+// }
+
+// console.log(color);
