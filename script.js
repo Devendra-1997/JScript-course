a5bbd7306a8f1b0d09b382ff4deb24594b171ec6
Devendra-1997 <devendradhakal92@gmail.com>
2023-12-26 04:34:50 +1100

    various operators in javascript learned

---
+++ script.js
@@ -0,0 +1,87 @@
+// Operator in js
+// used to perform some operation in data
+
+// Arithmetic Operators
+// let a = 5;
+// let b = 2;
+
+// console.log("a = ", a, " & b = ", b);
+// console.log("a + b = ", a + b);
+// console.log("a - b = ", a - b);
+// console.log("a * b = ", a * b);
+// console.log("a / b = ", a / b);
+
+// console.log("a % b = ", a % b); //modulus
+// console.log("a ** b = ", a ** b); //Exponentiation
+
+// Unary Operators
+// increment and decrement
+
+// let a = 5;
+// let b = 2;
+
+// console.log("a = ", a, " & b = ", b);
+// // a = a + 1;
+// // a++; post increment
+// // a--;
+// a = a - 1;
+// console.log("a = ", a);
+
+// ++a (post increment)
+
+// let a = 5;
+// let b = 2;
+
+// console.log("a = ", a, " & b = ", b);
+
+// console.log("++a ", ++a);
+// console.log("a = ", a);
+
+// Assignment Operators
+// to asign value
+
+// let a = 5;
+// let b = 2;
+
+// a += 4;
+// a -= 4;
+// a **= 4;
+// a %= 4;
+
+// console.log("a += ", a);
+// console.log("a -= ", a);
+// console.log("a %= ", a);
+// console.log("a **= ", a);
+
+// comparison operators
+
+// let a = 5;
+// let b = 2;
+
+// console.log("a === b", a === b);
+// console.log("a !== b", a !== b);
+
+// logical operators
+
+// logical &&
+
+// let a = 6;
+// let b = 4;
+
+// // let cond1 = a > b;
+// // let cond2 = a === 6;
+
+// console.log(" a = ", a, "& b = ", b);
+// // console.log("cond1 && cond2", cond1 && cond2);
+// console.log("cond1 && cond2", a > b && a === 6);
+
+// logical OR
+let a = 6;
+let b = 4;
+
+let cond1 = a > b;
+let cond2 = a === 6;
+
+console.log(" a = ", a, "& b = ", b);
+// console.log("cond1 && cond2", cond1 || cond2);
+console.log("cond1 && cond2", a > b && a === 6);
